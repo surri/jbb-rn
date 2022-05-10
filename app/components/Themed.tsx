96fb215e4c80d08a227936cbac9047f7576b2384
surri <thekeh@hotmail.com>
2022-05-10 18:25:13 +0900

    add theme for ScrollView & KeyboardAvodingView

--- app/components/Themed.tsx
+++ app/components/Themed.tsx
@@ -6,11 +6,15 @@
 import {
     Text as DefaultText,
     View as DefaultView,
+    ScrollView as DefaultScrollView,
     TouchableOpacity as DefaultTouchableOpacity,
+    KeyboardAvoidingView as DefaultKeyboardAvoidingView,
+    Platform,
 } from 'react-native'
 
 import Colors from '../theme'
 import { useTheme } from '@react-navigation/native'
+import Animated from 'react-native-reanimated'
 
 export function useThemeColor(
     props: { light?: string; dark?: string },
@@ -34,12 +38,13 @@ type ThemeProps = {
 
 export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
+export type ScrollViewProps = ThemeProps & DefaultScrollView['props'];
 export type TouchableOpacityProps = ThemeProps & DefaultTouchableOpacity['props'];
+export type KeyboardAvoidingViewProps = ThemeProps & DefaultKeyboardAvoidingView['props'];
 
 export function Text(props: TextProps) {
     const { style, lightColor, darkColor, ...otherProps } = props
     const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text')
-
     return <DefaultText style={[{ color }, style]} {...otherProps} />
 }
 
@@ -50,9 +55,29 @@ export function View(props: ViewProps) {
     return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />
 }
 
+export function ScrollView(props: ScrollViewProps) {
+    const { style, lightColor, darkColor, ...otherProps } = props
+    const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background')
+
+    return <DefaultScrollView style={[{ backgroundColor }, style]} {...otherProps} />
+}
+
 export function TouchableOpacity(props: TouchableOpacityProps) {
     const { style, lightColor, darkColor, ...otherProps } = props
     const borderColor = useThemeColor({ light: lightColor, dark: darkColor }, 'border')
 
     return <DefaultTouchableOpacity style={[{ borderColor, borderWidth: 1 }, style]} {...otherProps} />
 }
+export function KeyboardAvoidingView(props: KeyboardAvoidingViewProps) {
+    const { style, lightColor, darkColor, ...otherProps } = props
+    const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background')
+
+    return <DefaultKeyboardAvoidingView
+        style={[{ backgroundColor, borderWidth: 2 }, style]}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
+        {...otherProps}
+    />
+}
+
+
