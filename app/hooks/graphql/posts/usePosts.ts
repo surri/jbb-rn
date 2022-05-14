e498172c3b77565e59ac7f91d61fd98ab9bfa3ca
surri <thekeh@hotmail.com>
2022-05-14 23:16:14 +0900

    graphql hook

---
+++ app/hooks/graphql/posts/usePosts.ts
@@ -0,0 +1,38 @@
+import { gql, useQuery } from '@apollo/client'
+
+const POSTS_QUERY = gql`
+query variables($sportsId: Int, $keyword: String) {
+    posts(
+        sportsId: $sportsId
+        conditions:{
+            pagination: {
+                first: 10
+            }
+            keyword: $keyword
+        }
+    ) {
+        edges {
+            node {
+                title
+                contents
+            }
+            cursor
+        }
+        pageInfo {
+            startCursor
+            endCursor
+            hasPrevPage
+            hasNextPage
+        }
+    }
+}
+`
+
+type Virables = {
+    sportsId: number,
+    keyword: string,
+}
+
+export default function usePosts(variables: Virables) {
+    return useQuery(POSTS_QUERY, { variables })
+}
