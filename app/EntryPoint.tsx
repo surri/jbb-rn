f5d7ae2092272b90519734223c07e47ed935a51a
surri <thekeh@hotmail.com>
2022-05-21 22:30:22 +0900

    client ts

--- app/EntryPoint.tsx
+++ app/EntryPoint.tsx
@@ -2,48 +2,17 @@ import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import useCachedResources from './hooks/useCachedResources'
-import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
-import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
+import { ApolloProvider } from '@apollo/client'
 import {
     RecoilRoot,
 } from 'recoil'
-import { DarkTheme, DefaultTheme } from '@react-navigation/native'
-import theme from './theme'
+import client from './config/client'
 
 export default function App() {
-
     const isLoadingComplete = useCachedResources()
-    const colorScheme = useColorScheme()
+    // const colorScheme = useColorScheme()
 
-    const lightTheme = {
-        ...DefaultTheme,
-        colors: theme.light,
-    }
-    const darkTheme = {
-        ...DarkTheme,
-        colors: theme.dark,
-    }
-    const httpLink = createHttpLink({
-        uri: 'https://api.jangbibbal.com/graphql',
-    })
-
-    const authLink = setContext((_, { headers }) => {
-        const tempToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImV1bmciLCJkaXNwbGF5X25hbWUiOiJldW5nIiwiZW1haWwiOiJldW5nQGV1bmcuY29tIiwiaWF0IjoxNjQxMjE5NjE5LCJleHAiOjE2NDEzMDYwMTl9.3r_He6DOOPDdrweaxUW3nsKlHeq-L9IlkVk2t9Vtse8'
-        // get the authentication token from local storage if it exists
-        const token = tempToken //localStorage.getItem('token')
-        return {
-            headers: {
-                ...headers,
-                authorization: token ? `Bearer ${token}` : '',
-            },
-        }
-    })
-    const client = new ApolloClient({
-        link: authLink.concat(httpLink),
-        cache: new InMemoryCache(),
-    })
     if (!isLoadingComplete) {
         return null
     } else {
@@ -51,9 +20,7 @@ export default function App() {
             <ApolloProvider client={client}>
                 <SafeAreaProvider>
                     <RecoilRoot>
-                        <Navigation theme={colorScheme === 'dark' ? darkTheme : lightTheme}></Navigation>
-                        {/* <Navigation theme={lightTheme} /> */}
-                        {/* <Navigation theme={darkTheme} /> */}
+                        <Navigation/>
                         <StatusBar />
                     </RecoilRoot>
                 </SafeAreaProvider>
--- app/config/client.ts
+++ app/config/client.ts
@@ -1,17 +1,19 @@
 import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { createUploadLink } from 'apollo-upload-client'
+// const uri = 'https://api.jangbibbal.com/graphql'
+const uri = 'http://localhost:3004/graphql'
 
 const uploadLink = (createUploadLink({
-    uri: 'http://localhost:3004/graphql',
+    uri,
 }) as unknown) as ApolloLink
 
 const requestLink = createHttpLink({
-    uri: 'http://localhost:3004/graphql',
+    uri,
 })
 
 const authLink = setContext((_, { headers }) => {
-    const tempToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJldW5nIiwiZGlzcGxheU5hbWUiOiJFdW5nIiwiZW1haWwiOiJFdW5nQGhvdHBsZXIuY29tIiwiaWF0IjoxNjUyODQ4MjY3LCJleHAiOjE2NTI5MzQ2Njd9.kISfhmxcLjlPQxp5rPDQWPvj7v0YVTEAeqJVASVrob4'
+    const tempToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJldW5nIiwiZGlzcGxheU5hbWUiOiJFdW5nIiwicGhvbmUiOiIwMTA5MDk2NDA0MiIsImVtYWlsIjoiRXVuZ0Bob3RwbGVyLmNvbSIsImlhdCI6MTY1MjkyNDg2MiwiZXhwIjoxNjUzMDExMjYyfQ.utqtDLDHXJk18dZ76zec-J4qcx1M5lHzbNfyVm-HqrY'
     // get the authentication token from local storage if it exists
     const token = tempToken //localStorage.getItem('token')
     return {
